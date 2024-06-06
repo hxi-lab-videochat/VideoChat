7690709cbad0b014918db026d6bd60741c765248
manatoyamamto <32145099@fukuchiyama.ac.jp>
2024-06-06 13:34:36 +0900

    d

--- VideoChat-guest/js/script.js
+++ VideoChat-guest/js/script.js
@@ -393,7 +393,7 @@ at=false;
         const targetpeerId = data.msg;
         console.log('挙手したユーザーのPeerID:', targetpeerId);
         //var cv = document.querySelectorAll('.content video');
-        document.querySelectorAll('.content video').forEach(video => {
+        document.querySelectorAll('.contents video').forEach(video => {
           video.style.visibility = 'hidden';
         });
         const targetVideo = remoteVideos.querySelector(`[data-peer-id="${targetpeerId}"]`);
@@ -418,7 +418,7 @@ at=false;
       if (data.pn === 'lowerhand') {
         const targetpeerId = data.msg;
         console.log('手を下げたユーザーのPeerID:', targetpeerId);
-        var cv = document.querySelectorAll('.content video');
+        var cv = document.querySelectorAll('.contents video');
         // 最初のCSSスタイル書き換え
         cv.forEach(cv => {
           cv.style.visibility = 'hidden';
