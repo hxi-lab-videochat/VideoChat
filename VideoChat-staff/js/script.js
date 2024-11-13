83a96e69e3dfe725af7b25d14c2cd2fe79a7932c
hashimo <hkdp01hashi@gmail,com>
2024-11-14 03:34:59 +0900

    join usename

--- VideoChat-staff/js/script.js
+++ VideoChat-staff/js/script.js
@@ -444,7 +444,7 @@ at=false;
       if(true){
         console.log(userm());
       }
-      var s_msg='--'+userm()+":_"+localText.value+'--';/*`${userm()}:${localText.value}`;*/
+      var s_msg='//'+userm()+":_"+localText.value;/*`${userm()}:${localText.value}`;*/
       room.send(s_msg);
       var my_msgid=document.createElement('div');
       my_msgid.setAttribute('id',`${userm()}`)
